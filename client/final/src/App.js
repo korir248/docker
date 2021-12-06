1488aca6843e6ad96efee771fe5313fca12105a6
unknown <=>
2021-12-06 13:35:19 +0300

    fix: code fix

--- client/final/src/App.js
+++ client/final/src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          et <code>srcp</code> ad save 
+          Just another React App
+          
         </p>
         <a
 
